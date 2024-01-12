73b9942701b8aeefa23c0169d5175d92d288d827
ekrem35 <anliekrem35@gmail.com>
2024-01-12 14:03:11 +0300

    add icon assets

---
+++ app/router/icons/index.ts
@@ -0,0 +1,17 @@
+import daha from './dahaIcon.png';
+
+import home from './home.png';
+import homeActive from './home_active.png';
+
+import wallet from './wallet.png';
+import walletActive from './wallet_active.png';
+
+export default {
+  daha,
+
+  home,
+  homeActive,
+
+  wallet,
+  walletActive,
+};
